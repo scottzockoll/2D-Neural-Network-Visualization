95fbd9d2037e4b3f7c3a9510532a5663e1beec86
scottzockoll <32425272+scottzockoll@users.noreply.github.com>
2020-12-21 18:52:03 -0500

    draws and clears regions upon train

--- sketches/2dNNVisualization.js
+++ sketches/2dNNVisualization.js
@@ -3,6 +3,7 @@ import p5 from 'p5'
 
 
 export const twoDimNeuralNetwork = (canvas_size, model) => {
+    const BACKGROUND_COLOR = 250
     const CANVAS_SIZE = canvas_size
     let active_class = 1
     let predictionMode = false
@@ -13,17 +14,19 @@ export const twoDimNeuralNetwork = (canvas_size, model) => {
         active_class = active_class === 1 ? 0 : 1
     }
 
+    function getCanvasPoints(x, y) {
+        return [x + CANVAS_SIZE / 2, CANVAS_SIZE / 2 - y]
+    }
+
     function getCartesianPoints(x, y) {
         return [x - CANVAS_SIZE / 2, CANVAS_SIZE / 2 - y]
     }
 
-    function getSequence(res) {
+    function getArithmeticSequence(res) {
         let difference = (CANVAS_SIZE / res) / (res - 1)
-        let result = []
+        let result = [0]
         for (let i = 0; i < CANVAS_SIZE; i += difference) {
-            if (i === 0) {
-                result.push(0)
-            } else {
+            if (i !== 0) {
                 result.push(result[result.length - 1] + difference)
             }
         }
@@ -33,27 +36,58 @@ export const twoDimNeuralNetwork = (canvas_size, model) => {
     const mouseClicked = (s, e) => {
         if (predictionMode) {
             let [x, y] = getCartesianPoints(e.offsetX, e.offsetY)
-            console.log(x, y)
             model.predict(tf.tensor2d([x, y], [1, 2])).print()
         } else {
-            let color
-            if (active_class === 1) {
-                color = 'green'
-            } else {
-                color = 'red'
-            }
-            s.fill(color)
+            drawPoint(s, e.offsetX, e.offsetY, active_class)
             let [x, y] = getCartesianPoints(e.offsetX, e.offsetY)
             xs.push([x, y])
             ys.push(active_class)
-            console.log(x, y)
-            s.circle(e.offsetX, e.offsetY, 10)
         }
+    }
+
+    // x and y are in canvas points
+    const drawPoint = (s, x, y, classNumber) => {
+        s.stroke(0)
+        let color
+        if (classNumber === 1) {
+            color = 'green'
+        } else {
+            color = 'red'
+        }
+        s.fill(color)
+        s.circle(x, y, 10)
+    }
+
+    const drawLines = (s) => {
+        s.stroke(0)
+        s.fill(0, 250)
+        s.line(CANVAS_SIZE / 2, 0, CANVAS_SIZE / 2, CANVAS_SIZE)
+        s.line(0, CANVAS_SIZE / 2, CANVAS_SIZE, CANVAS_SIZE / 2)
+    }
+
+    const drawDataset = (s) => {
+        s.stroke(0)
+        for (let i = 0; i < xs.length; i += 1) {
+            let [x1, x2] = getCanvasPoints(...xs[i])
+            drawPoint(s, x1, x2, ys[i])
+        }
+    }
+
+    const clearCanvas = (s) => {
+        s.fill(BACKGROUND_COLOR, 250)
+        s.noStroke()
+        s.rect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+    }
+
+    const clearRegions = (s) => {
+        clearCanvas(s)
+        drawLines(s)
+        drawDataset(s)
     }
 
     const drawRegions = (s) => {
 
-        let [sequence, difference] = getSequence(10)
+        let [sequence, difference] = getArithmeticSequence(10)
 
         let cartesianMesh = []
         let canvasMesh = []
@@ -83,39 +117,43 @@ export const twoDimNeuralNetwork = (canvas_size, model) => {
             }
         })
 
-        model.predict(tf.tensor2d([[0, 0]], [1, 2])).print()
-
     }
 
-    function train(epochs) {
+    function train(s, epochs) {
+        s.trainButton.attribute('disabled', 'true')
         model.fit(tf.tensor2d(xs, [xs.length, 2]), tf.oneHot(ys, 2), {
             epochs: epochs,
         }).then(() => {
             console.log('Model trained')
+            clearRegions(s)
+            drawRegions(s)
+            s.trainButton.removeAttribute('disabled')
         });
+
     }
+
 
     const sketch = (s) => {
 
-
         s.setup = () => {
+            // create canvas
             let canvas = s.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
             canvas.parent('p5jsdiv');
             canvas.mouseClicked((e) => mouseClicked(s, e))
-            s.background(150)
-            s.line(CANVAS_SIZE / 2, 0, CANVAS_SIZE / 2, CANVAS_SIZE)
-            s.line(0, CANVAS_SIZE / 2, CANVAS_SIZE, CANVAS_SIZE / 2)
+
+            // draw graphics
+            s.background(BACKGROUND_COLOR)
+            drawLines(s)
+
+            // define buttons
             let changeClassButton = s.createButton('Change class');
             changeClassButton.mousePressed(() => changeClass())
-            let trainButton = s.createButton('Train');
-            trainButton.mousePressed(() => train(500))
-            let drawRegionsButton = s.createButton('Draw regions')
-            drawRegionsButton.mousePressed(() => drawRegions(s))
+            s.trainButton = s.createButton('Train');
+            s.trainButton.mousePressed(() => train(s,300))
 
         };
 
         s.keyPressed = (e) => {
-            console.log(s)
             if (e.key === 'p') {
                 predictionMode = predictionMode !== true
                 console.log(`Prediction mode: ${predictionMode}`)
